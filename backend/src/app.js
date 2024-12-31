58d56ecdbf87915175307bd4d2f81722b0b8c886
Thamannakc <147067089+Thamannakc@users.noreply.github.com>
2024-12-31 16:23:25 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,16 +3,38 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
+const vendorRoutes = require('./routes/vendor');
+const orderRoutes = require('./routes/order');
+const Product = require('./models/Product');
+const product = require('./routes/product');
 
 dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
 
-app.use(cors());
 app.use(express.json());
 
 //routes
 app.use('/api/auth',authRoutes);
+app.use('/api/smartshop/vendors', vendorRoutes);
+app.use('/api/smartshop/orders', orderRoutes);
+app.use('/api/smartshop/',product)
+
+
+app.get('/api/smartshop/category/:category', async (req, res, next) => {
+  try {
+    const { category } = req.params;
+    const categoryData = await Product.find({ category });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = app;
